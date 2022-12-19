07a77fef8cfc4f8844038e96649d2dbd4907bdf6
ReDay Zarra <redayzarra@gmail.com>
2022-12-18 19:21:55 -0500

    Adding notes on constant time complexity

---
+++ big-o.py
@@ -0,0 +1,16 @@
+"""
+O(1) - Constant time, meaning that the input size will not affect the time
+it takes for the algorithm to run.
+"""
+#Arrays
+numbers = [1, 2, 3, 4] # Creating an array
+numbers.append(4) # Adding to the end of the array
+numbers.pop() # Removing from the end of the array
+numbers[0] # Looking up numbers at a given index
+
+#Hashmaps / Hashsets
+hashmap = {} # Creating the hashmap
+hashmap["key"] = 10 # Adding to the hashmap
+"key" in hashmap # Returning the value from hashmaps
+hashmap["key"] # Returning values from hashmaps
+hashmap.pop("key") # Removing keys in hashmaps
