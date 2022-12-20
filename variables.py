0d11a17f5f0767154faf3ef8e254ae4c722d03ca
ReDay Zarra <redayzarra@gmail.com>
2022-12-19 22:15:01 -0500

    Adding notes on variables

---
+++ variables.py
@@ -0,0 +1,30 @@
+"""
+Python - is a dynamic language meaning we don't have to declare types
+"""
+n = 0
+print("n =", n)
+
+n = 'abc'
+print('n =', n)
+
+
+"""
+Multiple Assignments - declare multiple variables side by side
+"""
+n, m = 1, "abc"
+print(n, m)
+
+
+"""
+Incrementing - adding a constant value to the variable on every iteration
+"""
+n = n + 1
+
+n += 1
+
+"""
+Null - the null value in Python is just called None
+"""
+y = 6
+y = None
+print('y = ', y)
