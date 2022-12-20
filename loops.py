03c9fa5b0cd616e98fe5ebede6ffb29f871cb9e5
ReDay Zarra <redayzarra@gmail.com>
2022-12-19 22:24:50 -0500

    Adding notes on loops

---
+++ loops.py
@@ -0,0 +1,17 @@
+"""
+While loops also need to be indented
+"""
+n = 0
+while n < 5:
+  n += 1
+  print(n)
+
+
+"""
+For loops also use similar format
+"""
+for index in range(6): # The range goes from 0 to 5
+  print(index)
+
+for num in range(5, 1, -1): # The range starts from 5 and goes down to 2
+  print(num)
