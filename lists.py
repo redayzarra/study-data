1ece1fb00790beee16ce164a915408d342bc10c5
ReDay Zarra <redayzarra@gmail.com>
2022-12-21 19:26:18 -0500

    Adding notes on list comprehension

---
+++ lists.py
@@ -0,0 +1,8 @@
+"""
+List comprehension
+"""
+arr = [i for i in range(5)] # Creates an array with from 0 to 4
+print(arr)
+
+array = [i + i for i in range(5)] # For every index, double the index and add it to the array
+print(array)
